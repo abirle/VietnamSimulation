df6d60f622f92b2fc20df2e3c79247d7a8143dad
abirle <abirle@ufl.edu>
2026-04-08 12:03:28 -0400

    scene changes, interaction point focuses, resource management and decision screen

---
+++ VietnamSimulation/Assets/Scripts/InputFieldUpdate.cs
@@ -0,0 +1,66 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class InputFieldUpdate : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    public UnityEngine.UI.Slider slider;
+    int resourcesAllocated;
+    GameObject gameManager;
+    ResourceManager resourceManager;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("Manager");
+        resourceManager = gameManager.GetComponent<ResourceManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateInputFieldM1()
+    {
+        inputField.text = slider.value.ToString();
+        resourcesAllocated = int.Parse(inputField.text);
+        resourceManager.SetM1Resources(resourcesAllocated);
+
+    }
+
+
+    public void UpdateInputFieldM2()
+    {
+        inputField.text = slider.value.ToString();
+        resourcesAllocated = int.Parse(inputField.text);
+        resourceManager.SetM2Resources(resourcesAllocated);
+    }
+
+
+    public void UpdateInputFieldM3()
+    {
+        inputField.text = slider.value.ToString();
+        resourcesAllocated = int.Parse(inputField.text);
+        resourceManager.SetM3Resources(resourcesAllocated);
+    }
+
+
+    public void UpdateInputFieldM4()
+    {
+        inputField.text = slider.value.ToString();
+        resourcesAllocated = int.Parse(inputField.text);
+        resourceManager.SetM4Resources(resourcesAllocated);
+    }
+
+
+    public void UpdateInputFieldM5()
+    {
+        inputField.text = slider.value.ToString();
+        resourcesAllocated = int.Parse(inputField.text);
+        resourceManager.SetM5Resources(resourcesAllocated);
+    }
+}
