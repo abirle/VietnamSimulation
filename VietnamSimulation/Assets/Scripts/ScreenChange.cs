505c19ab6429b6a5e9886d2bad3378f0988bf154
abirle <abirle@ufl.edu>
2026-04-27 15:46:15 -0400

    POI glow, ledger slide

--- VietnamSimulation/Assets/Scripts/ScreenChange.cs
+++ VietnamSimulation/Assets/Scripts/ScreenChange.cs
@@ -10,16 +10,34 @@ public class ScreenChange : MonoBehaviour
     public GameObject ledgerSliderScreen;
     public GameObject decisionScreen;
     public GameObject resultsScreen;
+    public GameObject corkboardScreen;
+    public GameObject pauseScreen;
+
+
     public Camera camera;
     public Image blackscreen;
     Color transparent = new Color(0, 0, 0, 0);
 
 
-    bool isZoomingInOnMap;
+    bool isZoomingInOnMap = false;
+    bool isZoomingOutOnMap = false;
+    bool isZoomingInOnLedger = false;
+    bool isZoomingOutOnLedger = false;
+    bool isZoomingInOnCorkboard = false;
+    bool isZoomingOutOnCorkboard = false;
+    bool isViewingLedger = false;
+    bool isViewingMap = false;
+
     Vector3 cameraCenter = new Vector3(1288, 725, -1500);
     Vector3 cameraMap = new Vector3(1283, 722, -1500);
+    Vector3 cameraCorkboard = new Vector3(1284, (float)725.7, -1500);
+    Vector3 cameraLedger = new Vector3(1306, 725, -1500);
     float timeElapsed = 0f;
+    float secondTimeElapsed = 0f;
     float zoomDuration = 3f;
+    float fadeOutDuration = 2f;
+    float fadeInDuration = 2f;
+    float shiftDuration = 1f;
     
 
     GameObject gameManager;
@@ -31,29 +49,140 @@ public class ScreenChange : MonoBehaviour
     {
         gameManager = GameObject.FindGameObjectWithTag("Manager");
         resourceManager = gameManager.GetComponent<ResourceManager>();
+        militaryGoalsScreen.SetActive(true);
+        pauseScreen.SetActive(true);
+        ledgerSliderScreen.SetActive(true);
+        mapSliderScreen.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+// Update is called once per frame
+void Update()
     {
         if (isZoomingInOnMap)
         {
             timeElapsed += Time.deltaTime;
-            float progress = Mathf.Clamp01(timeElapsed / zoomDuration);
-            camera.transform.position = Vector3.Lerp(cameraCenter, cameraMap, progress);
-            camera.orthographicSize = Mathf.Lerp(5, 2, progress);
-            blackscreen.color = Color.Lerp(transparent, Color.black, progress);
+            float zoomProgress = Mathf.Clamp01(timeElapsed / zoomDuration);
+            float fadeOutProgress = Mathf.Clamp01(timeElapsed / fadeOutDuration);
 
-            if (progress == 1f)
+            camera.transform.position = Vector3.Lerp(cameraCenter, cameraMap, zoomProgress);
+            camera.orthographicSize = Mathf.Lerp(5, 2, zoomProgress);
+            blackscreen.color = Color.Lerp(transparent, Color.black, fadeOutProgress);
+
+            if (zoomProgress == 1f)
             {
-                isZoomingInOnMap = false;
+                secondTimeElapsed += Time.deltaTime;
                 militaryGoalsScreen.SetActive(false);
                 mapSliderScreen.SetActive(true);
-                camera.transform.position = new Vector3(1288, 725, -1500);
+                camera.transform.position = cameraCenter;
                 camera.orthographicSize = 5;
-                blackscreen.color = transparent;
+                float fadeInProgress = Mathf.Clamp01(secondTimeElapsed / fadeInDuration);
+                float extraTimeProgress = Mathf.Clamp01(secondTimeElapsed / (fadeInDuration + 1));
+                blackscreen.color = Color.Lerp(Color.black, transparent, fadeInProgress);
+
+                if (extraTimeProgress == 1f)
+                {
+                    isZoomingInOnMap = false;
+                }
             }
         }
+
+        else if (isZoomingOutOnMap)
+        {
+            timeElapsed += Time.deltaTime;
+            float fadeOutProgress = Mathf.Clamp01(timeElapsed / fadeOutDuration);
+            float extraTimeProgress = Mathf.Clamp01(timeElapsed / (fadeOutDuration + 1));
+            blackscreen.color = Color.Lerp(transparent, Color.black, fadeOutProgress);
+
+            if (extraTimeProgress == 1f)
+            {
+                secondTimeElapsed += Time.deltaTime;
+                mapSliderScreen.SetActive(false);
+                militaryGoalsScreen.SetActive(true);
+                float fadeInProgress = Mathf.Clamp01(secondTimeElapsed / fadeInDuration);
+                blackscreen.color = Color.Lerp(Color.black, transparent, fadeInProgress);
+
+                if (fadeInProgress == 1f)
+                {
+                    isZoomingOutOnMap = false;
+                }
+            }
+        }
+
+        else if (isZoomingInOnCorkboard)
+        {
+            timeElapsed += Time.deltaTime;
+            float zoomProgress = Mathf.Clamp01(timeElapsed / zoomDuration);
+            float fadeOutProgress = Mathf.Clamp01(timeElapsed / fadeOutDuration);
+            camera.transform.position = Vector3.Lerp(cameraCenter, cameraCorkboard, zoomProgress);
+            camera.orthographicSize = Mathf.Lerp(5, 2, zoomProgress);
+            blackscreen.color = Color.Lerp(transparent, Color.black, fadeOutProgress);
+
+            if (zoomProgress == 1f)
+            {
+                secondTimeElapsed += Time.deltaTime;
+                militaryGoalsScreen.SetActive(false);
+                corkboardScreen.SetActive(true);
+                camera.transform.position = cameraCenter;
+                camera.orthographicSize = 5;
+                float fadeInProgress = Mathf.Clamp01(secondTimeElapsed / fadeInDuration);
+                float extraTimeProgress = Mathf.Clamp01(secondTimeElapsed / (fadeInDuration + 1));
+                blackscreen.color = Color.Lerp(Color.black, transparent, fadeInProgress);
+
+                if (extraTimeProgress == 1f)
+                {
+                    isZoomingInOnCorkboard = false;
+                }
+            }
+        }
+
+        else if (isZoomingOutOnCorkboard)
+        {
+            timeElapsed += Time.deltaTime;
+            float fadeOutProgress = Mathf.Clamp01(timeElapsed / fadeOutDuration);
+            float extraTimeProgress = Mathf.Clamp01(timeElapsed / (fadeOutDuration + 1));
+            blackscreen.color = Color.Lerp(transparent, Color.black, fadeOutProgress);
+
+            if (extraTimeProgress == 1f)
+            {
+                secondTimeElapsed += Time.deltaTime;
+                corkboardScreen.SetActive(false);
+                militaryGoalsScreen.SetActive(true);
+                float fadeInProgress = Mathf.Clamp01(secondTimeElapsed / fadeInDuration);
+                blackscreen.color = Color.Lerp(Color.black, transparent, fadeInProgress);
+
+                if (fadeInProgress == 1f)
+                {
+                    isZoomingOutOnCorkboard = false;
+                }
+            }
+        }
+
+        else if (isViewingLedger)
+        {
+            timeElapsed += Time.deltaTime;
+            float shiftProgress = Mathf.Clamp01(timeElapsed / shiftDuration);
+
+            camera.transform.position = Vector3.Lerp(cameraCenter, cameraLedger, shiftProgress);
+
+            if (shiftProgress == 1f)
+            {
+                isViewingLedger = false;
+            }
+        }
+
+        else if (isViewingMap)
+        {
+            timeElapsed += Time.deltaTime;
+            float shiftProgress = Mathf.Clamp01(timeElapsed / shiftDuration);
+
+            camera.transform.position = Vector3.Lerp(cameraLedger, cameraCenter, shiftProgress);
+
+            if (shiftProgress == 1f)
+            {
+                isViewingMap = false;
+            }
+        }
+
     }
 
     public void MilitaryScene()
@@ -64,31 +193,65 @@ public class ScreenChange : MonoBehaviour
 
     public void MapZoomIn()
     {
-        timeElapsed = 0f;
-        isZoomingInOnMap = true;
+        if (!isZoomingInOnMap)
+        {
+            timeElapsed = 0f;
+            secondTimeElapsed = 0f;
+            isZoomingInOnMap = true;
+        }
     }
 
     public void MapZoomOut()
     {
-        camera.orthographicSize = 5;
-        camera.transform.position = new Vector3(1288, 725, -1500);
-
-        mapSliderScreen.SetActive(false);
-        militaryGoalsScreen.SetActive(true);
+        if (!isZoomingOutOnMap)
+        {
+            timeElapsed = 0f;
+            secondTimeElapsed = 0f;
+            isZoomingOutOnMap = true;
+        }
     }
 
 
     public void LedgerZoomIn()
     {
-        militaryGoalsScreen.SetActive(false);
-        ledgerSliderScreen.SetActive(true);
+        if (!isViewingLedger && !isZoomingInOnMap)
+        {
+            timeElapsed = 0f;
+            secondTimeElapsed = 0f;
+            isViewingLedger = true;
+        }
     }
 
 
     public void LedgerZoomOut()
     {
-        ledgerSliderScreen.SetActive(false);
-        militaryGoalsScreen.SetActive(true);
+        if (!isViewingMap)
+        {
+            timeElapsed = 0f;
+            secondTimeElapsed = 0f;
+            isViewingMap = true;
+        }
+    }
+
+
+    public void CorkboardZoomIn()
+    {
+        if (!isZoomingInOnCorkboard)
+        {
+            timeElapsed = 0f;
+            secondTimeElapsed = 0f;
+            isZoomingInOnCorkboard = true;
+        }
+    }
+
+    public void CorkboardZoomOut()
+    {
+        if (!isZoomingOutOnCorkboard)
+        {
+            timeElapsed = 0f;
+            secondTimeElapsed = 0f;
+            isZoomingOutOnCorkboard = true;
+        }
     }
 
 
--- VietnamSimulation/Assets/Scripts/SliderUpdate.cs
+++ VietnamSimulation/Assets/Scripts/SliderUpdate.cs
@@ -12,34 +12,81 @@ public class SliderUpdate: MonoBehaviour
     public UnityEngine.UI.Slider alternateSlider;
     public TMP_InputField alternateInputField;
 
+    GameObject sourceParent;
+    AudioSource audioSource;
+    AudioClip paperSlide;
+    bool slidePlaying;
+    float slideDuration = 2f;
+
     int resourcesAllocated;
     GameObject gameManager;
     ResourceManager resourceManager;
-    
-    
+    ScreenChange screenChange;
+
+    float timeElapsed = 0f;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
+    void Start()
     {
-        Debug.Log("awake");
         gameManager = GameObject.FindGameObjectWithTag("Manager");
+        sourceParent = GameObject.FindGameObjectWithTag("AudioSource");
         resourceManager = gameManager.GetComponent<ResourceManager>();
+        screenChange = resourceManager.GetComponent<ScreenChange>();
+        audioSource = sourceParent.GetComponent<AudioSource>();
+        paperSlide = audioSource.clip;
+        //screenChange.pauseScreen.SetActive(false);
+        //screenChange.ledgerSliderScreen.SetActive(false);
+        //screenChange.mapSliderScreen.SetActive(false);
+
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        timeElapsed += Time.deltaTime;
+        float slideProgress = Mathf.Clamp01(timeElapsed / slideDuration);
+
+        if (slideProgress == 1f)
+        {
+            slidePlaying = false;
+            timeElapsed = 0f;
+            slideProgress = 0f;
+        }
 
     }
 
 
     public void UpdateSliderM1()
     {
+        //if ((resourceManager.numResources - float.Parse(inputField.text)) < 0)
+        //{
+        //    float excessResources = float.Parse(inputField.text) - resourceManager.numResources;
+        //    slider.value = resourceManager.numResources;
+        //    alternateSlider.value = resourceManager.numResources;
+        //    alternateInputField.text = resourceManager.numResources.ToString();
+        //}
+        //else
+        //{
+        //    slider.value = float.Parse(inputField.text);
+        //    alternateSlider.value = float.Parse(inputField.text);
+        //    alternateInputField.text = inputField.text;
+        //}
+
         slider.value = float.Parse(inputField.text);
-        alternateSlider.value = float.Parse(inputField.text); 
+        alternateSlider.value = float.Parse(inputField.text);
         alternateInputField.text = inputField.text;
 
         resourcesAllocated = (int)slider.value;
         resourceManager.SetM1Resources(resourcesAllocated);
+
+        if (!slidePlaying)
+        {
+            audioSource.PlayOneShot(paperSlide);
+            slidePlaying = true;
+        }
 
     }
 
@@ -53,6 +100,12 @@ public class SliderUpdate: MonoBehaviour
         resourcesAllocated = (int)slider.value;
         resourceManager.SetM2Resources(resourcesAllocated);
 
+        if (!slidePlaying)
+        {
+            audioSource.PlayOneShot(paperSlide);
+            slidePlaying = true;
+        }
+
     }
 
 
@@ -64,7 +117,6 @@ public class SliderUpdate: MonoBehaviour
 
         resourcesAllocated = (int)slider.value;
         resourceManager.SetM3Resources(resourcesAllocated);
-
     }
 
 
@@ -76,7 +128,6 @@ public class SliderUpdate: MonoBehaviour
 
         resourcesAllocated = (int)slider.value;
         resourceManager.SetM4Resources(resourcesAllocated);
-
     }
 
 
@@ -88,6 +139,12 @@ public class SliderUpdate: MonoBehaviour
 
         resourcesAllocated = (int)slider.value;
         resourceManager.SetM5Resources(resourcesAllocated);
+
+        if (!slidePlaying)
+        {
+            audioSource.PlayOneShot(paperSlide);
+            slidePlaying = true;
+        }
 
     }
 }
